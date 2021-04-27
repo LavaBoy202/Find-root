7eb5298e3dd5ac00f731e636a4b3b364b2bbd7e5
LavanNithi <lnithian@uwaterloo.ca>
2021-04-27 14:40:13 -0400

    Create root.c

---
+++ root.c
@@ -0,0 +1,31 @@
+#include "root.h"
+#include <assert.h>
+
+double bisection(double (*f)(double), double lo, double hi, double tolerance) {
+  assert (tolerance > 0);
+  assert (f);
+  assert (lo < hi);
+  assert (f(hi) != 0);
+  assert (f(lo) != 0);
+  if (f(lo) > 0) {
+    assert(f(hi) < 0);
+  }
+  if (f(lo) < 0) {
+    assert (f(hi) > 0);
+  }
+  double midpoint = ((lo + hi) / 2);
+  while ((f(midpoint) < (tolerance * -1)) || (f(midpoint) > tolerance)) {
+    if ((f(midpoint) > 0) && (f(lo) > 0)) {
+      lo = midpoint;
+    }
+    else if ((f(midpoint) < 0) && (f(lo) < 0)) {
+      lo = midpoint;
+    }
+    else if (((f(midpoint) > 0) && (f(hi) > 0)) ||
+             ((f(midpoint) < 0) && (f(hi) < 0))) {
+      hi = midpoint;
+    }
+    midpoint = ((lo + hi) / 2);
+  }
+  return midpoint;
+}
